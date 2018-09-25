67ec0b0cdd370388d834a1a08fae9d123261b7fa
SashaJohnson <alexjohnson@yandex.ru>
2018-09-25 18:50:17 +0300

    initial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  
+  var divider = 5;
+  var pieces = [];
+  
+  while (number / divider > 0) {
+      
+      fragment = number / divider;
+      divider = divider * 5;
+      pieces.push(Math.round(fragment));
+      
+  
+}
+
+//console.log(pieces);
+
+var sum = 0;
+for (var i = 0; i < pieces.length; i++) {
+    
+    sum = sum + pieces[i];
+
+}
+return sum;
+//console.log(sum);
+
 }
