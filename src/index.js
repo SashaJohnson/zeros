9d3a8232810a44102d974d6f18fc3b0f779a5386
SashaJohnson <alexjohnson@yandex.ru>
2018-09-25 19:13:04 +0300

    removed unnecessary outputs, cleaned code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 module.exports = function getZerosCount(number) {
   
   var divider = 5;
-  var piecesRaw = [];
   var pieces = [];
   
   while (number / divider > 1) {
       
       fragment = number / divider;
       divider = divider * 5;
-      piecesRaw.push(fragment);
       pieces.push(Math.floor(fragment));
   
 }
 
-console.log(piecesRaw);
-console.log(pieces);
-
-
 var sum = 0;
 for (var i = 0; i < pieces.length; i++) {
-    
+ 
     sum = sum + pieces[i];
-
 }
 return sum;
-//console.log(sum);
 
 }
